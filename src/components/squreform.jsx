22f83e005fbe25a7dbe7ae6d12942dd99b1b4472
Dmitry Sinitsin <sinitsin.dmitry2013@yandex.ru>
2024-11-18 10:15:42 +0300

    Перемещаемая рыбка фикс

--- src/components/squreform.jsx
+++ src/components/squreform.jsx
@@ -5,8 +5,8 @@ function SqureForm(props){
     const nsqure=useRef()
     return(
         <div style={{display:"flex", flexDirection:"column", gap:20, alignItems:"center"}}>
-            <Input1 type={"number"} ref={nsqure} placeholder={`длина квадрата`}/>
-            <button onClick={()=>{props.callback({size:nsqure.current.value,key:(props.squres.length+1)})}} style={{height:50, width:50+"%", fontSize:25,userSelect:"none", cursor:"pointer"}}>добавить квадрат</button>
+            <Input1 type={"number"} ref={nsqure} placeholder={`размер рыбки`}/>
+            <button onClick={()=>{props.callback({size:nsqure.current.value,key:(props.squres.length+1)})}} style={{height:50, width:50+"%", fontSize:25,userSelect:"none", cursor:"pointer"}}>добавить рыбку</button>
             <div style={{display:"flex", gap:20}}><Squre_from_massive massive={props.squres}/></div>
         </div>
     )
